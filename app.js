597f260f3bc010cae547f79a1271f79f67559caa
yogesh.chaudhari.77 <chaudhari.yogesh20@gmail.com>
2021-04-04 17:09:00 +1000

    Create music table and populating the a2.json task complete

--- app.js
+++ app.js
@@ -3,10 +3,26 @@ const express = require('express');
 // System requires
 const os = require("os");
 const path = require('path');
+var fs = require('fs');
 
 // Third part requires
 const formidableMiddleware = require('express-formidable');
 const cookieParser = require('cookie-parser');
+const { v4: uuidv4 } = require('uuid')
+
+//AWS Requires
+var AWS = require("aws-sdk");
+
+// Config Requires
+var aws_config = require("./config/aws_admin_credentials_for_programmatic_access.js");
+
+AWS.config.update({
+    region: "ap-southeast-2",
+    accessKeyId: aws_config.acess_key_id,
+    secretAccessKey: aws_config.secret_access_key
+});
+
+var dynamodb = new AWS.DynamoDB();
 
 // Init the express app
 const app = express();
@@ -31,6 +47,82 @@ app.get("/", (req, res) => {
     res.render("index");
 });
 
+/**
+ * Creates a music table programatically
+ * Id is the primaru key. There is no sorting key
+ * Doubts : Documentation says that there is no need to provide other attributes beforehand it being schemaless. Is my understanding correct?
+ */
+app.get("create_music_table", (req, res) => {
+    var params = {
+        TableName: "music",
+        KeySchema: [
+            { AttributeName: "id", KeyType: "HASH" }  //Partition key
+            // No sorting key provided
+        ],
+
+        // Datatypes of the KeySchema attributes only. No need to provide other attributes before hand.
+        AttributeDefinitions: [
+            { AttributeName: "id", AttributeType: "S" }
+            // { AttributeName: "title", AttributeType: "S" },
+            // { AttributeName: "artist", AttributeType: "S" },
+            // { AttributeName: "year", AttributeType: "S" },          // Keeping it as a number for safer use
+            // { AttributeName: "web_url", AttributeType: "S" },
+            // { AttributeName: "image_url", AttributeType: "S" }
+        ],
+        ProvisionedThroughput: {
+            ReadCapacityUnits: 10,
+            WriteCapacityUnits: 10
+        }
+    };
+
+    dynamodb.createTable(params, function (err, data) {
+        if (err) {
+            console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+        }
+    });
+
+});
+// End of create music table
+
+/**
+ * Loads the sample data from the a2.json file into the DynamoDB
+ * Need to improvise and change pitItem request from callback to sync
+ */
+app.get("/populate_music_table", (req, res) => {
+
+    var music_objects = JSON.parse(fs.readFileSync('a2.json', 'utf8'));
+
+    for (let i = 0, music_objetcts_len = music_objects["songs"].length; i < music_objetcts_len; i++) {
+
+        // Forming a item for the put request
+        const params = {
+            TableName: "music",
+            Item: {
+                id: { S: "" + uuidv4() },                      //=> '1a1ghf4d-3z7d-4bad-10ss-2b9f5b3dfb6d'
+                title: { S: "" + music_objects["songs"][i].title },
+                artist: { S: "" + music_objects["songs"][i].artist },
+                year: { S: "" + music_objects["songs"][i].year },
+                web_url: { S: "" + music_objects["songs"][i].web_url },
+                img_url: { S: "" + music_objects["songs"][i].img_url }
+            }
+        };
+
+        dynamodb.putItem(params, function (err, data) {
+            if (err) {
+                console.log("err", err);
+            }
+            else {
+                console.log("data", data)
+            }
+        });
+    }
+
+    res.send("<pre>Database loaded</pre>");
+});
+// End of populate_music_table
+
 // User auth routes
 app.get("/login", (req, res) => {
     res.render("auth/login");
@@ -41,8 +133,8 @@ app.get("/signup", (req, res) => {
 });
 
 // 404 page 
-// app.use( (req, res) => {
-//   res.render("templates/404.ejs");
-// } );
+app.use((req, res) => {
+    res.render("pages/404.ejs");
+});
 
 module.exports = app;
---
+++ config/sample_aws_admin_credentials_for_programmatic_access copy.js
@@ -0,0 +1,16 @@
+
+/**
+ * AWS config object
+ * The credentials in this file will be passed before making call to the AWS services
+ * Make sure that these credentials are not shared with any body
+ */
+const aws_config = {
+    "username" : "",
+    "password" : "",
+    "acess_key_id" : "",
+    "secret_access_key" : "",
+    "region" : "",
+    "console_login_link" : "" 
+};
+
+module.exports = aws_config;
